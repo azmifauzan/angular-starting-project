315c8995e1b1f6c3a452b1e2c1fe7ae1de052993
Fauzan Azmi <azmifauzan@gmail.com>
2025-12-19 16:57:53 +0700

    Make selected user reactive using signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Header } from "./component/header/header";
 import { User } from "./component/user/user";
+import { DUMMY_USERS } from './class/dummy-users';
+import { Task } from "./component/task/task";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  users = DUMMY_USERS;
   protected readonly title = signal('starting-project');
+  selectedUserid = signal('u1');
+  selectedUser = computed(() => {
+    return this.users.find(user => user.id === this.selectedUserid())!;
+  });
+
+  onUserSelected(userId: string) {
+    this.selectedUserid.set(userId);
+  }
 }
